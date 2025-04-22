bbe7194037392bc533db21ce111369d9e4c939a4
hemang-shimpi <hemangshimpi@outlook.com>
2025-04-22 19:27:15 -0400

    created stock search screen and routed it to main file

---
+++ stock_trading_app/lib/screens/stock_search_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class StockSearchScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Search Stocks")),
+      body: Center(child: Text("Search for stocks...")),
+    );
+  }
+}
