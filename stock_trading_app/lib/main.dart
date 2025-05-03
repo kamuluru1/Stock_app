145fad6ffd2400a3e89055a22aa546a2fe88cc8a
HemangShimpi <54321009+HemangShimpi@users.noreply.github.com>
2025-05-03 19:53:50 -0400

    final commit

--- stock_trading_app/lib/main.dart
+++ stock_trading_app/lib/main.dart
@@ -1,3 +1,4 @@
+// StockHub By Hemang Shimpi and Krishna Amuluru
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
